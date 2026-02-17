e36a6ff2610f5ef446ce0ed60406103429e91c84
twcoo <mflesterc@gmail.com>
2026-02-17 08:56:21 +0800

    chore: lint codes

--- home/tests/Timeline.spec.ts
+++ home/tests/Timeline.spec.ts
@@ -1,11 +1,19 @@
-import { mount } from '@vue/test-utils'
-import { describe, it, expect } from 'vitest'
-import { NFlex, NCard, NH2, NH3, NH4, NTimeline, NTimelineItem } from 'naive-ui'
-import Timeline from '@/components/Timeline.vue'
-import timeline from '@/data/timeline'
-import { Briefcase } from '@vicons/ionicons5'
+import { mount } from "@vue/test-utils";
+import { describe, it, expect } from "vitest";
+import {
+  NFlex,
+  NCard,
+  NH2,
+  NH3,
+  NH4,
+  NTimeline,
+  NTimelineItem,
+} from "naive-ui";
+import Timeline from "@/components/Timeline.vue";
+import timeline from "@/data/timeline";
+import { Briefcase } from "@vicons/ionicons5";
 
-describe('Timeline.vue', () => {
+describe("Timeline.vue", () => {
   const wrapper = mount(Timeline, {
     global: {
       components: {
@@ -19,32 +27,32 @@ describe('Timeline.vue', () => {
         Briefcase,
       },
     },
-  })
+  });
 
-  it('renders Naive UI components', () => {
-    expect(wrapper.findComponent(NFlex).exists()).toBe(true)
-    expect(wrapper.findComponent(NCard).exists()).toBe(true)
-    expect(wrapper.findComponent(NH2).exists()).toBe(true)
-    expect(wrapper.findComponent(NH3).exists()).toBe(true)
-    expect(wrapper.findComponent(NH4).exists()).toBe(true)
-    expect(wrapper.findComponent(NTimeline).exists()).toBe(true)
-    expect(wrapper.findComponent(NTimelineItem).exists()).toBe(true)
-  })
+  it("renders Naive UI components", () => {
+    expect(wrapper.findComponent(NFlex).exists()).toBe(true);
+    expect(wrapper.findComponent(NCard).exists()).toBe(true);
+    expect(wrapper.findComponent(NH2).exists()).toBe(true);
+    expect(wrapper.findComponent(NH3).exists()).toBe(true);
+    expect(wrapper.findComponent(NH4).exists()).toBe(true);
+    expect(wrapper.findComponent(NTimeline).exists()).toBe(true);
+    expect(wrapper.findComponent(NTimelineItem).exists()).toBe(true);
+  });
 
-  it('renders ionicons5 components', () => {
-    expect(wrapper.findComponent(Briefcase).exists()).toBe(true)
-  })
+  it("renders ionicons5 components", () => {
+    expect(wrapper.findComponent(Briefcase).exists()).toBe(true);
+  });
 
-  it('renders timeline items correctly', () => {
-    const items = wrapper.findAllComponents(NTimelineItem)
+  it("renders timeline items correctly", () => {
+    const items = wrapper.findAllComponents(NTimelineItem);
 
     items.forEach((itemWrapper, idx) => {
-      const item = timeline[idx]
+      const item = timeline[idx];
 
-      expect(itemWrapper.props('time')).toBe(`${item.year} - ${item.duration}`)
-      expect(itemWrapper.props('content')).toBe(item.details)
-      expect(itemWrapper.text()).toContain(item.title)
-      expect(itemWrapper.text()).toContain(item.company)
-    })
-  })
-})
+      expect(itemWrapper.props("time")).toBe(`${item.year} - ${item.duration}`);
+      expect(itemWrapper.props("content")).toBe(item.details);
+      expect(itemWrapper.text()).toContain(item.title);
+      expect(itemWrapper.text()).toContain(item.company);
+    });
+  });
+});
